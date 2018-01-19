6339c67379cf90f3edc52ecb7e077608c8e84d33
Pascal Brokmeier <accounts@pascalbrokmeier.de>
2018-01-19 12:31:03 +0100

    improved TOC generation

--- assets/custom.js
+++ assets/custom.js
@@ -12,17 +12,4 @@ function makeTOC(){
     }
 }
 
-if(window.attachEvent) {
-    window.attachEvent('onload', yourFunctionName);
-} else {
-    if(window.onload) {
-        var curronload = window.onload;
-        var newonload = function(evt) {
-            curronload(evt);
-            makeTOC(evt);
-        };
-        window.onload = newonload;
-    } else {
-        window.onload = makeTOC;
-    }
-}
+document.addEventListener("DOMContentLoaded", makeTOC)
